e959163e502829a0ac236c5be2978133cd9071ef
Bill.Conn <Bill.Conn@usd.edu>
2015-03-19 18:24:35 -0500

    Initial Commit of Form Tests repo

---
+++ TestRemoteLoadDataGrid/TestRemoteLoadDataGrid/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TestRemoteLoadDataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.techBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.electricutilityDataSet = new TestRemoteLoadDataGrid.electricutilityDataSet();
+            this.techTableAdapter = new TestRemoteLoadDataGrid.electricutilityDataSetTableAdapters.techTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.techBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricutilityDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(532, 207);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.techBindingSource;
+            this.comboBox1.DisplayMember = "name";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(175, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.ValueMember = "techId";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // techBindingSource
+            // 
+            this.techBindingSource.DataMember = "tech";
+            this.techBindingSource.DataSource = this.electricutilityDataSet;
+            // 
+            // electricutilityDataSet
+            // 
+            this.electricutilityDataSet.DataSetName = "electricutilityDataSet";
+            this.electricutilityDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // techTableAdapter
+            // 
+            this.techTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 273);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.techBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricutilityDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private electricutilityDataSet electricutilityDataSet;
+        private System.Windows.Forms.BindingSource techBindingSource;
+        private electricutilityDataSetTableAdapters.techTableAdapter techTableAdapter;
+    }
+}
+
---
+++ TestRemoteLoadDataGrid/TestRemoteLoadDataGrid/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TestRemoteLoadDataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'electricutilityDataSet.tech' table. You can move, or remove it, as needed.
+            this.techTableAdapter.Fill(this.electricutilityDataSet.tech);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox selection = (ComboBox)sender;
+            int itemKey = (int) selection.SelectedValue;
+            Console.WriteLine(itemKey);
+            getTechTickets(itemKey);
+        }
+
+        private void getTechTickets(int techId)
+        {
+            try
+            {
+                string MyConnection2 = "server=127.0.0.1;user id=electric_util_ad;password=eleadmin;database=electricutility;";
+                //Display query
+                string Query = "select ticketId AS Ticket, onDate AS Date, am AS AM, pm AS PM  from schedule natural join tech where techId = "+techId+";";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                //  MyConn2.Open();
+                //For offline connection we weill use  MySqlDataAdapter class.
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable; // here i have assign dTable object to the dataGridView1 object to display data.             
+                // MyConn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
